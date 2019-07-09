06b389baecc42e3836691a0ef4c3a7933e7fcfc3
trhung <trh.bkdn>
2019-07-09 19:08:44 +0700

    - Them chuc nang cua Floating button la chuyen sang activity clouda data.

--- app/src/main/java/com/example/utilitymanagement/MainActivity.java
+++ app/src/main/java/com/example/utilitymanagement/MainActivity.java
@@ -54,14 +54,23 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-//        FloatingActionButton fab = findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
+        final Intent intent = new Intent(this, cloudData.class);
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
 //                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
 //                        .setAction("Action", null).show();
-//            }
-//        });
+
+                Bundle bundle = new Bundle();
+                bundle.putString("machine", spinnerMachine.getSelectedItem().toString());
+                bundle.putString("dateOfCheck", spinnerDate.getSelectedItem().toString());
+
+                intent.putExtra("Data", bundle);
+
+                startActivity(intent);
+            }
+        });
         DrawerLayout drawer = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
